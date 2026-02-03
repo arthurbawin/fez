1d484261e6548ff6f807b06172f30aa8bf445153
Joan Le Pouhaër <joan2810@login3.int.fir.alliancecan.ca>
2026-02-02 17:15:41 -0800

    Update run scripts and prm files (post_processing)

--- buildrelease/Monolithic_fsi3.sh
+++ buildrelease/Monolithic_fsi3.sh
@@ -9,5 +9,4 @@
 #SBATCH --output=/home/joan2810/scratch/monolithic_fsi3/log/output.txt
 #SBATCH --error=/home/joan2810/scratch/monolithic_fsi3/log/error.txt
 
-export OMP_NUM_THREADS=3
 srun --cpu-bind=cores ./monolithic_fsi ../data/parameters_file/monolithic_fsi3_3D.prm

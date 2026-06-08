94b37785bd44e46cc44f60b7cd914833e60c6181
arthurbawin <arthurbawin@gmail.com>
2026-06-08 16:58:06 -0400

    SUPG stabilization for the incompressible NS equations

--- include/scratch_data.h
+++ include/scratch_data.h
@@ -71,20 +71,25 @@ namespace NavierStokesScratch
     ns_only = 0,
 
     /**
+     * Add the contribution of the full divergence form to the SUPG residual.
+     */
+    divergence_form = 1 << 0,
+
+    /**
      * Reinit data to assemble the mesh movement elasticity equation.
      */
-    pseudo_solid = 1 << 0,
+    pseudo_solid = 1 << 1,
 
     /**
      * Reinit data to assemble constraints using a Lagrange multiplier.
      */
-    lagrange_multiplier = 1 << 1,
+    lagrange_multiplier = 1 << 2,
 
     /**
      * Reinit data to assemble the Cahn-Hilliard system, used to form the CHNS
      * system.
      */
-    cahn_hilliard = 1 << 2,
+    cahn_hilliard = 1 << 3,
 
     /**
      * Reinit data to assemble the compressible Navier-Stokes system (including
@@ -1215,6 +1220,8 @@ namespace NavierStokesScratch
     unsigned int t_lower;
 
   public:
+    static constexpr bool with_divergence_form =
+      (update_flags & divergence_form) != 0;
     static constexpr bool enable_pseudo_solid =
       (update_flags & pseudo_solid) != 0;
     static constexpr bool enable_lagrange_multiplier =
